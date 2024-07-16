52e5c3d565dbc10479930724a86613103ca904f9
prabhatkapse <prabhatkapse@gmail.com>
2024-07-16 16:53:41 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -1,19 +1,69 @@
+using System.Linq;
+
 public class StringCalculator
 {
-  public int Add(string input)
-  {
-    if (string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input))
+    public int Add(string input)
     {
-    return 0;
+        if (checkNullString(input))
+            return 0;
+
+        var numberList = inputDecoder(input);
+        var negativeNumbers = checkNegativeNumber(numberList);
+
+        if (negativeNumbers.Count > 0)
+            throw new NegativeNumberException($"Negatives not allowed: { string.Join(", ", negativeNumbers) }");
+
+        return numberList.Sum();
     }
 
-    if (input == 0)
+    private static bool checkNullString(string input)
+    { return string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input); }
+
+    private static List<int> checkNegativeNumber(List<int> numberList) 
     {
-      return 0;
+        var negativeNumbers = new List<int>();
+
+        foreach (var number in numberList)
+        {
+            if (number < 0)
+            {
+                negativeNumbers.Add(number);
+            }
+        }
+        return negativeNumbers;
     }
-    return -1;
-  }
-   
+    private static List<int> inputDecoder(string numbers)
+    {
+        string[] delimiters = new string[] { ",", "\n" };
+        if (numbers.StartsWith("//"))
+        {
+            int delimiterIndex = numbers.IndexOf('\n');
+            string customDelimiter = numbers.Substring(2, delimiterIndex - 2);
+            delimiters = new string[] { customDelimiter };
+            numbers = numbers.Substring(delimiterIndex + 1);
+        }
+
+        string[] numberStrings = numbers.Split(delimiters, StringSplitOptions.None);
+        List<int> numberList = new List<int>();
+
+        foreach (var numberString in numberStrings)
+        {
+            if (int.TryParse(numberString, out int number))
+            {
+                if (number <= 1000)
+                {
+                    numberList.Add(number);
+                }
+            }
+        }
+        return numberList;
+
+    }
 }
 
-  
+class NegativeNumberException : Exception
+{
+    public NegativeNumberException(string message) : base(message) { }
+}
+
+
